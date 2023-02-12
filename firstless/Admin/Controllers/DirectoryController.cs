52ff576749871501f648bc032f7ef3038fa2969c
bezve <katya.bezverkhova@mail.ru>
2023-02-13 05:44:10 +0600

    lesson

---
+++ firstless/Admin/Controllers/DirectoryController.cs
@@ -0,0 +1,45 @@
+﻿using Admin.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Admin.Controllers
+{
+    public class DirectoryController : Controller
+    {
+        private IWebHostEnvironment hostEnvironment;
+        public DirectoryController(IWebHostEnvironment hostEnvironment)
+        {
+            this.hostEnvironment = hostEnvironment;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult DirectoryRoomProperties(int page = 0)
+        {
+            return View();
+        }
+        [HttpPost]  
+        public IActionResult RoomProperties()
+        {
+            string NameProperties = Request.Form["NameProperties"];
+            string DescProperties = Request.Form["Description"];
+            return View();
+        }
+        [HttpPost]
+        public IActionResult RoomPropertiesModel(RoomProperties roomProperties)
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> RoomPropertiesModel(IFormFile photo)
+        {
+            using (var stream = new FileStream(Path.Combine(hostEnvironment.WebRootPath, photo.FileName), FileMode.OpenOrCreate))
+            {
+                await photo.CopyToAsync(stream);
+            }
+            string fileName = photo.FileName;
+            return View();
+
+    }
+    }
+}
